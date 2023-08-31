6e81754f041f642bae4a5609216ec72d079549ff
MariaEsquivelf <131416497+MariaEsquivelf@users.noreply.github.com>
2023-08-31 11:51:10 -0400

    Update item.py

--- lib/shopping_app/item.py
+++ lib/shopping_app/item.py
@@ -1,18 +1,17 @@
 class Item:
-    instances = []
     from ownable import set_owner
-    def __init__(self, name, price, owner):
+    instances = []
+    def __init__(self, name, price, owner=None):
         self.name = name
         self.price = price
         self.set_owner(owner)
-        # When an Item instance is created, that Item instance (self) is stored in a class variable called instances.
+        # Cuando se crea una instancia de Item (self), se agrega a la lista de instancias (instances) de la clase.
         Item.instances.append(self)
 
     def label(self):
-        return {"name": self.name, "price": self.price}
+        return {"nombre": self.name, "precio": self.price}  # Cambio de "name" y "price" a "nombre" y "precio"
 
     @staticmethod
     def item_all():
-        # Returns instances ==> Item.item_all() returns all Item instances created so far.
+        # Devuelve la lista de instancias generadas hasta ahora usando Item.item_all().
         return Item.instances
-
